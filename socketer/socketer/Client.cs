c75d5c025323dde1028467bc1df8c24f4701b3ff
Aaron <andersonaaronj@gmail.com>
2017-05-22 09:44:01 -0600

    Add files via upload
    A C# version of the client

---
+++ socketer/socketer/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebSocket4Net;
+
+namespace socketer
+{
+    class Client
+    {
+
+        String clientID;
+
+        static void Main(string[] args)
+        {
+            WebSocket socket = new WebSocket("ws://localhost");
+            socket.Open();
+            Console.ReadLine();
+            socket.Send("connection!");
+            Console.ReadLine();
+            socket.Close();
+        }
+    }
+}
